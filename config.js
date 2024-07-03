05d769c2d0b509e3f5c92f84950005adb01b5bfd
Stayred1337 <151662269+Stayred1337@users.noreply.github.com>
2024-07-03 11:02:22 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+   botToken: "YOUR_BOT_TOKEN", // Modify your bot TOKEN here
+   hostURL: "YOUR_URL", // Modify your host URL here
+};
+// for port forewording use "ssh -R 80:localhost:5000 serveo.net"
